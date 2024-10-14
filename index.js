8b5ac14a023454110f7d4d44d57ea2b777c13c91
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-14 15:02:53 +0200

    Allow configuring protocol

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const pack = require('bare-pack')
 
-module.exports = function (drive, entry, opts) {
-  return pack(new URL(entry, 'drive://'), opts, readModule)
+module.exports = function (drive, entry, opts = {}) {
+  const {
+    protocol = 'drive:'
+  } = opts
+
+  return pack(new URL(entry, protocol + '//'), opts, readModule)
 
   function readModule (url) {
     return drive.get(url.pathname)
