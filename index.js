d32b999df096ec36b7ff1286d5cd35dccbec871d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-01 15:52:59 +0100

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const pack = require('bare-pack')
 
-module.exports = function (drive, entry, opts = {}) {
-  const {
-    protocol = 'drive:'
-  } = opts
+module.exports = async function (drive, entry, opts = {}) {
+  const root = new URL('drive:///')
 
-  return pack(new URL(entry, protocol + '//'), opts, readModule)
+  const bundle = await pack(new URL(entry, root), opts, readModule, listPrefix)
 
-  function readModule (url) {
+  return bundle.unmount(root)
+
+  async function readModule (url) {
     return drive.get(url.pathname)
+  }
+
+  async function * listPrefix (url) {
+    if (url.pathname[url.pathname.length - 1] !== '/') url.pathname += '/'
+
+    for await (const { key } of drive.list(url.pathname, { recursive: true })) {
+      yield new URL(key, url)
+    }
   }
 }
--- test.js
+++ test.js
@@ -7,18 +7,41 @@ test('require', async (t) => {
   const bundle = await pack(new Localdrive('test/fixtures/require'), '/foo.js')
 
   const expected = new Bundle()
-    .write('drive:///baz.js', 'module.exports = 42\n', {
-      imports: {}
-    })
-    .write('drive:///bar.js', 'require(\'./baz.js\')\n', {
-      imports: {
-        './baz.js': 'drive:///baz.js'
-      }
-    })
-    .write('drive:///foo.js', 'require(\'./bar.js\')\n', {
+    .write('/foo.js', 'require(\'./bar.js\')\n', {
       main: true,
       imports: {
-        './bar.js': 'drive:///bar.js'
+        './bar.js': '/bar.js'
+      }
+    })
+    .write('/bar.js', 'require(\'./baz.js\')\n', {
+      imports: {
+        './baz.js': '/baz.js'
+      }
+    })
+    .write('/baz.js', 'module.exports = 42\n', {
+      imports: {}
+    })
+
+  t.alike(bundle, expected)
+})
+
+test('package.json#assets', async (t) => {
+  const bundle = await pack(new Localdrive('test/fixtures/package-json-assets'), '/foo.js')
+
+  const expected = new Bundle()
+    .write('/foo.js', '\n', {
+      main: true,
+      imports: {
+        '#package': '/package.json'
+      }
+    })
+    .write('/package.json', '{ "name": "foo", "assets": ["bar/"] }\n', {
+      imports: {}
+    })
+    .write('/bar/baz.txt', 'hello world\n', {
+      asset: true,
+      imports: {
+        '#package': '/package.json'
       }
     })
 
